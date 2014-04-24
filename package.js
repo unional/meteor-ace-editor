c6b704dc3ebc41fdaab044762f129d088b5f286b
Homa Wong <hwong@paloaltonetworks.com>
2014-04-24 11:38:51 -0700

    Load language tools as non-asset. Will add other files as needed.

--- package.js
+++ package.js
@@ -9,16 +9,20 @@ Package.on_use(function(api) {
     var files = fs.readdirSync(path.join(packagePath, "ace-builds", "src"));
     files.forEach(function(file) {
         if (file === "snippets") { return; }
-        api.add_files(path.join("ace-builds", "src", file), "client", {isAsset: true});
+        var filePath = path.join("ace-builds", "src", file);
+        console.log(filePath);
+        api.add_files(filePath, "client", {isAsset: true});
     });
 
     var snippets = fs.readdirSync(path.join(packagePath, "ace-builds", "src", "snippets"));
     snippets.forEach(function(file) {
-        api.add_files(path.join("ace-builds", "src", "snippets", file), "client", {isAsset: true});
+        var snippetPath = path.join("ace-builds", "src", "snippets", file);
+        console.log("snippet: " + snippetPath);
+        api.add_files(snippetPath, "client", {isAsset: true});
     });
 
     // aceConfig.js must load after ace.js. It configures the right path to load themes, mode, etc.
-    api.add_files(["ace-builds/src/ace.js", "ace-builds/src/ext-modelist.js", "aceConfig.js"], "client");
+    api.add_files(["ace-builds/src/ace.js", "ace-builds/src/ext-language_tools.js", "ace-builds/src/ext-modelist.js", "aceConfig.js"], "client");
 });
 
 Package.on_test(function(api) {
@@ -36,7 +40,7 @@ Package.on_test(function(api) {
     });
 
     // aceConfig.js must load after ace.js. It configures the right path to load themes, mode, etc.
-    api.add_files(["ace-builds/src/ace.js", "ace-builds/src/ext-modelist.js", "aceConfig.js"], "client");
+    api.add_files(["ace-builds/src/ace.js", "ace-builds/src/ext-language_tools.js", "ace-builds/src/ext-modelist.js", "aceConfig.js"], "client");
 
     api.add_files("packageTest.js", "client");
 });
